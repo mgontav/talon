b6e322a8c5039f82a587ebe2758520df4309a200
Miguel Tavares <mgontav@users.noreply.github.com>
2015-02-05 12:53:23 +0000

    Remove PyML installation from setup.py to prevent conflicts.

--- setup.py
+++ setup.py
@@ -100,4 +100,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] in ['develop', 'install']:
         enforce_executable('curl', 'sudo aptitude install curl')
 
-        install_pyml()
+        #install_pyml()
